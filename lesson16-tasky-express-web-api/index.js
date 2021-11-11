a2825a46b617b44fb9ec927fd43544086c9e1a7e
Miguel Gamboa <miguelgamboa@outlook.com>
2021-11-11 09:45:27 +0000

    minor fix

--- lesson16-tasky-express-web-api/index.js
+++ lesson16-tasky-express-web-api/index.js
@@ -3,11 +3,10 @@
 const express = require('express')
 const app = express()
 const tasksRouter = require('./lib/tasks-web-api')
-const tasks = require('./lib/tasks-db')
-const port = 3000
+const PORT = 3000
 
 app.use(tasksRouter)
 
-app.listen(port, () => {
-    console.log(`Tasky app listening on port ${port}!`)
+app.listen(PORT, () => {
+    console.log(`Tasky app listening on port ${PORT}!`)
 })

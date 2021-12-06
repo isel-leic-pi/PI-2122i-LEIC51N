63bf69bca771455b7e7928ce802a2281d3c38b40
Miguel Gamboa <miguelgamboa@outlook.com>
2021-12-06 22:45:09 +0000

    lesson 25

--- lesson25-tasky-ui-html/lib/tasky-router.js
+++ lesson25-tasky-ui-html/lib/tasky-router.js
@@ -2,12 +2,20 @@
 
 const express = require('express')
 const tasksRouter = require('./tasks-web-api')
+const path = require('path')
 
 /**
  * @param {Express} app 
  */
 module.exports = function(app) {
     app.use(express.json()) // Parses of HTTP request body and populates req.body
+    app.use(express.static('public'))
+    // app.get('/bootstrap.min.css', (req, res) => { res.sendFile(path.join(process.cwd(), 'bootstrap.min.css')) })
+    // app.get('/todo.png', (req, res) => { res.sendFile(path.join(process.cwd(), 'todo.png')) })
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(process.cwd(), 'views/index.html'))
+    })
     app.use(tasksRouter)
 
     // eslint-disable-next-line no-unused-vars
